3acf6e49956c62feb0ef36926e9b473249caf7f8
bokrosf <bokros.feri@hotmail.com>
2023-01-28 15:06:09 +0100

    Add project files.

---
+++ Source/SnakeGame/SnakeGame/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Controls;
+using SnakeGame.Views.Pages;
+
+namespace SnakeGame;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		MainPage = new StartupPage();
+	}
+}
---
+++ Source/SnakeGame/SnakeGame/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+
+namespace SnakeGame;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		MauiAppBuilder builder = MauiApp
+			.CreateBuilder()
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+
+		return builder.Build();
+	}
+}
