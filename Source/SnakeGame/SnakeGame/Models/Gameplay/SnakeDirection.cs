631ed949fbf81641a60962e6cb2a162002a72e2b
bokrosf <bokros.feri@hotmail.com>
2023-02-17 23:31:47 +0100

    SnakeDirection enum added to the project.

---
+++ Source/SnakeGame/SnakeGame/Models/Gameplay/SnakeDirection.cs
@@ -0,0 +1,27 @@
+namespace SnakeGame.Models.Gameplay;
+
+/// <summary>
+/// The direction where the snake is moving.
+/// </summary>
+internal enum SnakeDirection
+{
+    /// <summary>
+    /// Upward.
+    /// </summary>
+    Up,
+
+    /// <summary>
+    /// Downward.
+    /// </summary>
+    Down,
+    
+    /// <summary>
+    /// Leftward.
+    /// </summary>
+    Left,
+    
+    /// <summary>
+    /// Rightward.
+    /// </summary>
+    Right
+}
