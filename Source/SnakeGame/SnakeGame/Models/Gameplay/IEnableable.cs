2541e2de5f6e5044432c001f7dd17b38b7ca9540
bokrosf <bokros.feri@hotmail.com>
2023-02-18 19:21:52 +0100

    IEnableable added to project.

---
+++ Source/SnakeGame/SnakeGame/Models/Gameplay/IEnableable.cs
@@ -0,0 +1,12 @@
+namespace SnakeGame.Models.Gameplay;
+
+/// <summary>
+/// Defines members that components can implement to enable or disable their behaviours.
+/// </summary>
+public interface IEnableable
+{
+    /// <summary>
+    /// Gets or sets whether the item is enabled.
+    /// </summary>
+    bool Enabled { get; set; }
+}
