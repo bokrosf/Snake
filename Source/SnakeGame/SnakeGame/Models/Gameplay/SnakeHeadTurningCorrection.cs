ffd39d83c04a23e316ab77bec2eec5f41b7d5a12
bokrosf <bokros.feri@hotmail.com>
2023-02-19 21:44:36 +0100

    Documentation comment of SnakeHeadTurningCorrection class has been fixed.

--- Source/SnakeGame/SnakeGame/Models/Gameplay/SnakeHeadTurningCorrection.cs
+++ Source/SnakeGame/SnakeGame/Models/Gameplay/SnakeHeadTurningCorrection.cs
@@ -7,7 +7,7 @@ namespace SnakeGame.Models.Gameplay;
 /// </summary>
 /// <param name="HeadSegments">
 /// Snake body segments that represents the head turning. The first element is the attachment position where all the other segments joined to.
-/// The last element is the new head position. Always contains at least one element that is the head position.
+/// The last element is the new head position. Always contains the head position.
 /// </param>
 /// <param name="HeadDirection">Direction where the snake's head looking in.</param>
 public record SnakeHeadTurningCorrection(IReadOnlyList<Vector> HeadSegments, Vector HeadDirection);
