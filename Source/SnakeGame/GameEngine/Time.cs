8a154b54728b1ac953ee44feed47487659167969
bokrosf <bokros.feri@hotmail.com>
2023-02-25 15:31:24 +0100

    Time class added to GameEngine project.

---
+++ Source/SnakeGame/GameEngine/Time.cs
@@ -0,0 +1,12 @@
+namespace SnakeGame.GameEngine;
+
+/// <summary>
+/// Represents game time, and defines methods and properties to examine time changes.
+/// </summary>
+public class Time
+{
+    /// <summary>
+    /// Elapsed time since 
+    /// </summary>
+    public static float DeltaTime { get; private set; }
+}
